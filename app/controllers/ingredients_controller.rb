f22ebfa7e9e182882d430463087c1a0646008dbb
Adrian <adrigalileo@gmail.com>
2021-12-11 17:35:10 +0100

    Add ingredients controller and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,21 @@
+#frozen_string_literal: true
+
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: %i[show]
+
+  # GET /ingredients or /ingredients.json
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  # GET /ingredients/1 or /ingredients/1.json
+  def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+end
