d084c46008e03ab54452ebd8ee0c626cf42f0d9a
Adrian <adrigalileo@gmail.com>
2021-12-13 18:59:39 +0100

    Add carrierwave and image to ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Ingredient < ApplicationRecord
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_000735) do
+ActiveRecord::Schema.define(version: 2021_12_11_214554) do
 
   create_table "ingredients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_000735) do
     t.string "quantity_unit", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
