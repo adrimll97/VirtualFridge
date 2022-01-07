caf37663a89e2001fc77090679f60173a90ec959
Adrian <adrigalileo@gmail.com>
2022-01-07 18:29:10 +0100

    Show registration errors and set es as default locale

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module VirtualFridge
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.initialize_on_precompile = false
+
+    config.i18n.default_locale = :es
   end
 end
