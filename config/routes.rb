759a69ac5e039b905dc536a42d24a28feee98967
Adrian <adrigalileo@gmail.com>
2022-01-07 13:11:22 +0100

    Do not use modals to users views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, {
-    controllers: {
-      sessions: 'devise/my_session',
-      registrations: 'devise/my_registration',
-      passwords: 'devise/my_passwords'
-    }
-  }
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
   root to: 'home#index'
