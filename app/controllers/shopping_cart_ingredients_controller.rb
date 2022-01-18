4a992cf71e2721410619f158b9d4e50a0924e3aa
Adrian <adrigalileo@gmail.com>
2022-01-18 20:52:11 +0100

    Add update and destroy shopping cart ingredients

--- app/controllers/shopping_cart_ingredients_controller.rb
+++ app/controllers/shopping_cart_ingredients_controller.rb
@@ -9,9 +9,34 @@ class ShoppingCartIngredientsController < ApplicationController
     end
   end
 
+  def update
+    fi_params = update_shopping_cart_ingredients_params
+    begin
+      @shopping_cart_ingredient.update!(fi_params)
+      flash[:notice] = I18n.t(:edited_correctly, scope: :ingredients)
+    rescue StandardError => _e
+      flash[:alert] = @shopping_cart_ingredient.errors.full_messages
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    begin
+      @shopping_cart_ingredient.destroy!
+      flash[:notice] = I18n.t(:removed_from_fridge, scope: :ingredients)
+    rescue StandardError => _e
+      flash[:alert] = @shopping_cart_ingredient.errors.full_messages
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
   private
 
   def set_shopping_cart_ingredient
     @shopping_cart_ingredient = ShoppingCartIngredient.find(params[:id])
+  end
+
+  def update_shopping_cart_ingredients_params
+    params.require(:shopping_cart_ingredient).permit(:ingredient_id, :quantity_number)
   end
 end
