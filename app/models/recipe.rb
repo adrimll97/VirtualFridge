2de6181e50aed6ba4251752da628b3281d521557
Adrian <adrigalileo@gmail.com>
2022-01-20 19:44:29 +0100

    Recipe model migration (#25)
    * Add recipe and recipe ingredient migrations
    
    * Add recipe and recipe ingredient models

---
+++ app/models/recipe.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Recipe < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  serialize :steps, Array
+
+  belongs_to :user
+  has_many :recipe_ingredients, dependent: :destroy
+
+  validates :user, presence: true
+  validates :user, uniqueness: true
+end
