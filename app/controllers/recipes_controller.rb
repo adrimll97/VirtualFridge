8213c55223471b29c3855264c6c9f25243860f3e
Adrian <adrigalileo@gmail.com>
2022-01-23 15:29:46 +0100

    Add recipe show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,18 @@ RECIPES_PER_PAGE = 24
 
 # Controlador de recetas
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show]
+
   def index
     @recipes = Recipe.all.page(params[:page]).per(RECIPES_PER_PAGE)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
