88bf0d2089b67c35dba7863bbab8d4d1ec6b3e99
Adrian <adrigalileo@gmail.com>
2022-04-07 00:48:39 +0200

    Add visual information to expired and close to expired ingredients

--- app/helpers/fridge_ingredients_helper.rb
+++ app/helpers/fridge_ingredients_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 module FridgeIngredientsHelper
+  def expired_class(ingredient)
+    return 'expired_ingredient' if ingredient.expired?
+    return 'close_expired_ingredient' if ingredient.close_expired?
+
+    ''
+  end
 end
--- app/models/fridge_ingredient.rb
+++ app/models/fridge_ingredient.rb
@@ -6,4 +6,16 @@ class FridgeIngredient < ApplicationRecord
 
   validates :ingredient, uniqueness: { scope: :fridge, message: I18n.t(:duplicated_in_fridge, scope: :ingredients) }
   validates :fridge, :ingredient, :quantity_number, :quantity_unit, presence: true
+
+  def expired?
+    return false unless expiration_date.present?
+
+    Date.today > expiration_date
+  end
+
+  def close_expired?
+    return false unless expiration_date.present?
+
+    (Date.today...Date.today + 1.week).include?(expiration_date)
+  end
 end
