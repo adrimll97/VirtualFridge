37a4866afcd93955319603f21fe7f80473fa7841
Adrian <adrigalileo@gmail.com>
2022-05-18 22:26:51 +0200

    add menu show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,7 +3,22 @@
 MENUS_PER_PAGE = 20
 
 class MenusController < ApplicationController
+  before_action :set_menu, only: %i[show]
+
   def index
     @menus = Menu.all.page(params[:page]).per(MENUS_PER_PAGE)
+  end
+
+  def show
+    @lunchs_number = @menu.lunchs_number
+    @dinners_number = @menu.dinners_number
+    @lunchs_per_day = @menu.lunchs_per_day
+    @dinners_per_day = @menu.dinners_per_day
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -11,4 +11,35 @@ class Menu < ApplicationRecord
   def dinners_number
     menu_recipes.dinner.count
   end
+
+  def lunchs_per_day
+    {
+      0 => recipes_per_kind_day('lunch', 'monday'),
+      1 => recipes_per_kind_day('lunch', 'tuesday'),
+      2 => recipes_per_kind_day('lunch', 'wednesday'),
+      3 => recipes_per_kind_day('lunch', 'thursday'),
+      4 => recipes_per_kind_day('lunch', 'friday'),
+      5 => recipes_per_kind_day('lunch', 'saturday'),
+      6 => recipes_per_kind_day('lunch', 'sunday')
+    }
+  end
+
+  def dinners_per_day
+    {
+      0 => recipes_per_kind_day('dinner', 'monday'),
+      1 => recipes_per_kind_day('dinner', 'tuesday'),
+      2 => recipes_per_kind_day('dinner', 'wednesday'),
+      3 => recipes_per_kind_day('dinner', 'thursday'),
+      4 => recipes_per_kind_day('dinner', 'friday'),
+      5 => recipes_per_kind_day('dinner', 'saturday'),
+      6 => recipes_per_kind_day('dinner', 'sunday')
+    }
+  end
+
+  private
+
+  def recipes_per_kind_day(kind, day)
+    recipes = menu_recipes.send(kind).send(day)
+    recipes.map(&:recipe)
+  end
 end
