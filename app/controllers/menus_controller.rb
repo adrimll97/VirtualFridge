960b0725820f517d24486426836fbbf08ae842ef
Adrian <adrigalileo@gmail.com>
2022-05-19 20:16:47 +0200

    add menu destroy

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,7 +3,7 @@
 MENUS_PER_PAGE = 20
 
 class MenusController < ApplicationController
-  before_action :set_menu, only: %i[show]
+  before_action :set_menu, only: %i[show destroy]
 
   def index
     @menus = Menu.all.page(params[:page]).per(MENUS_PER_PAGE)
@@ -14,6 +14,15 @@ class MenusController < ApplicationController
     @dinners_number = @menu.dinners_number
     @lunchs_per_day = @menu.lunchs_per_day
     @dinners_per_day = @menu.dinners_per_day
+  end
+
+  def destroy
+    @menu.destroy!
+    flash[:notice] = I18n.t(:menu_destroy, scope: :menus)
+  rescue StandardError => _e
+    flash[:alert] = @menu.errors.full_messages
+  ensure
+    redirect_to user_path(current_user.id)
   end
 
   private
--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module MenuHelper
+  def self_menu?(menu)
+    menu.user_id == current_user&.id
+  end
 end
